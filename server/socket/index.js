a56f0597d8e901bc8619feefe141d29aa6c2b1e2
Da Woon Lee <daniel.lee9415@gmail.com>
2018-05-03 17:18:27 -0400

    added chatbox component and collab

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,16 @@
-module.exports = (io) => {
-  io.on('connection', (socket) => {
+module.exports = io => {
+  io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
+    })
+    socket.on('test', () => {
+      console.log('this is a test!!!!')
+    })
+    socket.on('sendMessage', data => {
+      console.log('this is a test', data)
+      socket.broadcast.emit('updateChat', data)
     })
   })
 }
