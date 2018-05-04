379acce56fcbb9efff8a21ba291e4b1347a3b55e
Ian Dewsbury <ianmichaeld@gmail.com>
2018-05-04 11:33:10 -0400

    Update GithubUpdates.js

--- client/components/GithubUpdates.js
+++ client/components/GithubUpdates.js
@@ -3,12 +3,6 @@ import axios from 'axios'
 
 export default class GitHubUpdates extends Component {
 
-  componentDidMount () {
-    axios.get('https://api.github.com/user/orgs?access_token=')
-    .then(data => console.log('DATA!!!', data))
-    .catch(err => console.log(err))
-  }
-
   render () {
     return (
       <h1>Github updates</h1>
