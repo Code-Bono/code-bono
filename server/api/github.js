7105fbe638e49230a5018ae139762d55547d044d
Ian Dewsbury <ianmichaeld@gmail.com>
2018-05-06 15:26:51 -0400

    install @octokit/rest

--- server/api/github.js
+++ server/api/github.js
@@ -1,80 +1,78 @@
 const router = require('express').Router()
-const createToken = require('./utils')
+const createToken = require('./utils').default.default
 const octokit = require('@octokit/rest')()
 module.exports = router
 
-
-
 router.get('/projects/columns/cards', (req, res, next) => {
-
-  let headers;
+  let headers
 
   createToken
-  .then(installationToken => {
-    headers = {
-      authorization: `token ${installationToken.data.token}`,
-      accept: 'application/vnd.github.inertia-preview+json'
-    }
-  })
-  .then(() => {
-    return octokit.projects.getRepoProjects({
-      headers,
-      owner: 'Code-Bono-Projects',
-      repo: 'code-bono-test-2'
+    .then(installationToken => {
+      headers = {
+        authorization: `token ${installationToken.data.token}`,
+        accept: 'application/vnd.github.inertia-preview+json'
+      }
     })
-  })
-  .then(repoProjects => {
-    // for now, just returning the first project in the repo
-    const projectId = repoProjects.data[0].id
-    return octokit.projects.getProjectColumns({
-      headers,
-      project_id: projectId
+    .then(() => {
+      return octokit.projects.getRepoProjects({
+        headers,
+        owner: 'Code-Bono-Projects',
+        repo: 'code-bono-test-2'
+      })
     })
-  })
-  .then(projectColumns => {
-    console.log('ALL COLUMNS', projectColumns)
-    const columnId = projectColumns.data[0].id
-    return octokit.projects.getProjectCards({
-      headers,
-      column_id: columnId
+    .then(repoProjects => {
+      // for now, just returning the first project in the repo
+      const projectId = repoProjects.data[0].id
+      return octokit.projects.getProjectColumns({
+        headers,
+        project_id: projectId
+      })
     })
-  })
-  .then(projectCards => {
-    // only sending the actual content of the card
-    const notes = projectCards.data.map(card => {
-      return card.note
+    .then(projectColumns => {
+      console.log('ALL COLUMNS', projectColumns)
+      const columnId = projectColumns.data[0].id
+      return octokit.projects.getProjectCards({
+        headers,
+        column_id: columnId
+      })
     })
-    res.send(notes)
-  })
-  .catch(next)
+    .then(projectCards => {
+      // only sending the actual content of the card
+      const notes = projectCards.data.map(card => {
+        return card.note
+      })
+      res.send(notes)
+    })
+    .catch(next)
 })
 
 router.post('/projects/:testProjectName', (req, res, next) => {
-
   const projectName = req.params.testProjectName
 
   // Use JWT to get installation id
-  octokit.apps.createInstallationToken({
+  octokit.apps
+    .createInstallationToken({
       headers: {
-      authorization: `Bearer ${token}`,
-      accept: 'application/vnd.github.machine-man-preview+json'
-    },
-    installation_id: 152649
-  })
-  // use installation token to access restricted api
-  .then(installationToken => {
-    octokit.projects.createRepoProject({
-      headers: {
-        authorization: `token ${installationToken.data.token}`,
-        accept: 'application/vnd.github.inertia-preview+json'
+        authorization: `Bearer ${token}`,
+        accept: 'application/vnd.github.machine-man-preview+json'
       },
-      owner: 'Code-Bono-Projects',
-      repo: 'code-bono-test1',
-      name: projectName
+      installation_id: 152649
     })
-    .then(result => {
-      res.json(result)
+    // use installation token to access restricted api
+    .then(installationToken => {
+      octokit.projects
+        .createRepoProject({
+          headers: {
+            authorization: `token ${installationToken.data.token}`,
+            accept: 'application/vnd.github.inertia-preview+json'
+          },
+          owner: 'Code-Bono-Projects',
+          repo: 'code-bono-test1',
+          name: projectName
+        })
+        .then(result => {
+          res.json(result)
+        })
     })
-  })
-  .catch(next)
+    .catch(next)
 })
--- server/api/utils.js
+++ server/api/utils.js
@@ -3,14 +3,16 @@ const jwt = require('jsonwebtoken')
 const octokit = require('@octokit/rest')()
 
 // generate JWT (sign with RSA SHA256)
-const cert = fs.readFileSync('/Users/yoni/Downloads/code-bono-projects.2018-05-04.private-key.pem')  // get private key
+const cert = fs.readFileSync(
+  '/Users/iandewsbury/Desktop/code-bono-projects.2018-05-04.private-key.pem'
+) // get private key
 const payload = {
   exp: Date.now() / 1000 + 100,
   iss: 11788
 }
-const token = jwt.sign(payload, cert, { algorithm: 'RS256'});
+const token = jwt.sign(payload, cert, { algorithm: 'RS256' })
 const createToken = octokit.apps.createInstallationToken({
-    headers: {
+  headers: {
     authorization: `Bearer ${token}`,
     accept: 'application/vnd.github.machine-man-preview+json'
   },
