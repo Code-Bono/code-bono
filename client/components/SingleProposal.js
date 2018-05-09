f1c89c93366fd07a63625b488d5ded5613346d55
Yoni Slotwinere <yonislots@gmail.com>
2018-05-09 15:59:54 -0400

    testing readme

---
+++ client/components/SingleProposal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Grid, Image, Card } from 'semantic-ui-react'
+
+export default class SingleProposal extends Component {
+
+  render() {
+    return (
+      <h1>SINGLE PROPOSAL</h1>
+    )
+  }
+}
