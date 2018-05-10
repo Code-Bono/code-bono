32228cebf01d32d5fb9e843988c2ca11fbdee6a5
Geena Gao <geena.gao@gmail.com>
2018-05-10 13:12:21 -0400

    Added eager loading and org into fo single proposal view

--- client/components/SingleProposal.js
+++ client/components/SingleProposal.js
@@ -15,11 +15,24 @@ export default class SingleProposal extends Component {
     const { singleProposal } = this.props
     return (
       <div>
-        <h1>{singleProposal.name}</h1>
-        <img src={singleProposal.image} />
-        <h2>Request description</h2>
-        <p>{singleProposal.description}</p>
-        <h3>Deadline: {singleProposal.deadline}</h3>
+        {singleProposal.id ? (
+          <div>
+            <h2>{singleProposal.organization.name.toUpperCase()}</h2>
+            <h1>Proposal: {singleProposal.name}</h1>
+            <img className="singlePageImg" src={singleProposal.image} />
+            <h2>Proposal description</h2>
+            <p>{singleProposal.description}</p>
+            <h3>Deadline: {singleProposal.deadline}</h3>
+            <h2>About the Organization:</h2>
+            <p>{singleProposal.organization.description}</p>
+            <h3>Get in touch with {singleProposal.organization.name}:</h3>
+            <p>Email: {singleProposal.organization.email}</p>
+            <p>Phone: {singleProposal.organization.phoneNumber}</p>
+            <p>Address: {singleProposal.organization.address}</p>
+          </div>
+        ) : (
+          <h2>Loading selected proposal...</h2>
+        )}
       </div>
     )
   }
--- server/db/models/proposal.js
+++ server/db/models/proposal.js
@@ -1,30 +1,42 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Proposal = db.define('proposal', {
-  name: {
-    type: Sequelize.STRING,
-    unique: true
+const Proposal = db.define(
+  'proposal',
+  {
+    name: {
+      type: Sequelize.STRING,
+      unique: true
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    timeframe: {
+      type: Sequelize.ENUM('hours', 'days', 'weeks', 'months')
+    },
+    deadline: {
+      type: Sequelize.DATEONLY,
+      allowNull: false
+    },
+    image: {
+      type: Sequelize.STRING,
+      defaultValue:
+        'https://organicthemes.com/demo/nonprofit/wp-content/themes/organic-nonprofit/images/logo.png'
+    },
+    isActive: {
+      type: Sequelize.BOOLEAN
+    }
   },
-  description: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  timeframe: {
-    type: Sequelize.ENUM('hours', 'days', 'weeks', 'months')
-  },
-  deadline: {
-    type: Sequelize.DATEONLY,
-    allowNull: false
-  },
-  image: {
-    type: Sequelize.STRING,
-    defaultValue:
-      'https://organicthemes.com/demo/nonprofit/wp-content/themes/organic-nonprofit/images/logo.png'
-  },
-  isActive: {
-    type: Sequelize.BOOLEAN
+  {
+    getterMethods: {
+      snippet() {
+        let sentences = this.description.split('.')
+        let firstSentence = sentences[0] + '.'
+        return firstSentence
+      }
+    }
   }
-})
+)
 
 module.exports = Proposal
