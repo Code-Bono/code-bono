b622996ac3176bd5b8d4fb2a13bce68c3418024a
Da Woon Lee <daniel.lee9415@gmail.com>
2018-05-10 15:05:45 -0400

    can assign causes to proposal now

--- script/seed.js
+++ script/seed.js
@@ -15,7 +15,8 @@ const {
   Chatroom,
   Message,
   Organization,
-  Proposal
+  Proposal,
+  Cause
 } = require('../server/db/models')
 
 const users = [
@@ -25,6 +26,13 @@ const users = [
   { email: 'gg@email.com', password: '123' },
   { email: 'ian@email.com', password: '123' },
   { email: 'yoni@email.com', password: '123' }
+]
+
+const causes = [
+  { name: 'Mentoring' },
+  { name: 'Arts' },
+  { name: 'Counselling' },
+  { name: 'Conservation' }
 ]
 
 const organizations = [
@@ -61,7 +69,8 @@ const proposals = [
     description:
       'We are looking for a team to help us migrate our website from Angular to React.',
     deadline: '2018-08-13',
-    image: 'https://s3-media2.fl.yelpcdn.com/bphoto/yzBRa3vDfi6rlh0TnqeByg/o.jpg',
+    image:
+      'https://s3-media2.fl.yelpcdn.com/bphoto/yzBRa3vDfi6rlh0TnqeByg/o.jpg',
     isActive: true,
     organizationId: 1
   },
@@ -70,7 +79,8 @@ const proposals = [
     description:
       'We are looking for a team to create a web app that will help us gather, screen, and onboard interested volunteers.',
     deadline: '2018-07-03',
-    image: 'http://hamelintrust.org.uk/wp-content/uploads/2015/07/volunteer-wordall.png',
+    image:
+      'http://hamelintrust.org.uk/wp-content/uploads/2015/07/volunteer-wordall.png',
     isActive: true,
     organizationId: 1
   },
@@ -87,7 +97,8 @@ const proposals = [
     description:
       'Help us build a mobile app that tracks cyborg bamboo deforestation.',
     deadline: '2018-10-17',
-    image: 'http://www.wormfood.co.uk/wp-content/uploads/2013/05/cyborgs-bamboo-bw.jpg',
+    image:
+      'http://www.wormfood.co.uk/wp-content/uploads/2013/05/cyborgs-bamboo-bw.jpg',
     isActive: true,
     organizationId: 2
   },
@@ -96,7 +107,8 @@ const proposals = [
     description:
       'Cyborg pandas are some of the most beautiful creatures in captivity. We are looking or a team to build a web app that helps zookeepers provide the best care for their specific cyborg needs.',
     deadline: '2018-08-13',
-    image: 'https://orig00.deviantart.net/b481/f/2013/136/e/5/cyborg_pandas_by_z_studios-d65htmk.png',
+    image:
+      'https://orig00.deviantart.net/b481/f/2013/136/e/5/cyborg_pandas_by_z_studios-d65htmk.png',
     isActive: true,
     organizationId: 2
   },
@@ -113,7 +125,8 @@ const proposals = [
     description:
       'We know that developers are good at typing, so we would like to find a team to type a bunch of things into our system, which will be the most efficient way of completing the task.',
     deadline: '2019-01-15',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuqkdNkU2FgaEss7GdUh2-YA2XXYuFEqVlxSHbqj58UN9tT_hNoA',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuqkdNkU2FgaEss7GdUh2-YA2XXYuFEqVlxSHbqj58UN9tT_hNoA',
     isActive: true,
     organizationId: 2
   }
@@ -124,6 +137,7 @@ const seed = async () => {
   await Organization.bulkCreate(organizations)
   await Chatroom.bulkCreate(chatrooms)
   await Proposal.bulkCreate(proposals)
+  await Cause.bulkCreate(causes)
 }
 
 const main = () => {
@@ -136,6 +150,7 @@ const main = () => {
       console.log(`seeded ${chatrooms.length} chatrooms`)
       console.log(`seeded ${organizations.length} organizations`)
       console.log(`seeded ${proposals.length} proposals`)
+      console.log(`seeded ${causes.length} causes`)
       console.log(`seeded successfully`)
       return seed()
     })
