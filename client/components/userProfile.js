578b04d92827680dfffef1b15e3caf5b32877661
Yoni Slotwinere <yonislots@gmail.com>
2018-05-15 17:39:27 -0400

    styling for user profile page

--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -32,24 +32,27 @@ export default class UserProfile extends Component {
             <Item.Group>
               {projects.map(project => {
                 return (
-                  <Item key={project.id}>
-                    <div className="content">
-                      <a className="header">{project.name}</a>
-                      <div className="description">
-                        {project.description}
-                      </div>
-                    </div>
-                    <div>
-                      <Link to={`/projects/${project.id}`}>
-                        <Button primary id="user-profile-browse"
-                          primary
-                          onClick={evt => this.props.loadProject(project.id)}
-                        >
-                          <Icon name="code" />Check in on this project
-                        </Button>
-                      </Link>
-                    </div>
-                  </Item>
+                  <List divided verticalAlign='middle' key={project.id}>
+                    <List.Item id="user-profile-project-list">
+                      <List.Content floated='right'>
+                        <Link to={`/projects/${project.id}`}>
+                          <Button primary id="user-profile-browse"
+                            primary
+                            onClick={evt => this.props.loadProject(project.id)}
+                          >
+                            <Icon name="code" />Check in on this project
+                          </Button>
+                        </Link>
+                        </List.Content>
+
+                        <List.Header ><Icon name="marker"/>
+                          {project.name}
+                      </List.Header>
+                        <List.Description>
+                          {project.description}
+                      </List.Description>
+                    </List.Item>
+                  </List>
                 )
               })}
             </Item.Group>
@@ -68,71 +71,3 @@ export default class UserProfile extends Component {
     )
   }
 }
-
-
-      // <Container>
-      //   <Header as="h1" icon textAlign="center">
-      //   {user ? (
-      //     <div>
-      //       <Header.Content id="user-profile-title">
-      //         {user.fullname || user.email}
-      //       </Header.Content>
-      //       <Header as="h1">
-      //         <Image size="massive" src={user.imageUrl} />
-      //       </Header>
-      //      </div>
-      //     ) : (
-      //       <h3 className="loading">Loading current user...</h3>
-      //     )}
-
-      //   </Header>
-
-      //     <h2 id="user-profile-projects-header">Your Projects</h2>
-
-      //     {projects.length ? (
-      //       <Item.Group>
-      //         {projects.map(project => {
-      //           return (
-      //             <Item key={project.id}>
-      //               <div className="content">
-      //                 <a className="header">{project.name}</a>
-      //                 <div className="description">
-      //                   {project.description.slice(0, 80)}...
-      //                 </div>
-      //               </div>
-      //               <div>
-      //                 <Link to={`/projects/${project.id}`}>
-      //                   <Button
-      //                     primary
-      //                     onClick={evt => this.props.loadProject(project.id)}
-      //                   >
-      //                     <Header as="h3">Check in on this project</Header>
-      //                   </Button>
-      //                 </Link>
-      //               </div>
-      //             </Item>
-      //           )
-      //         })}
-      //       </Item.Group>
-      //     ) : (
-      //       <div>
-      //         <List divided verticalAlign='middle'>
-      //           <List.Item>
-      //             <div className="no-active-projects">
-
-      //               <List.Content>
-      //                 You currently have no active projects
-      //               </List.Content>
-      //             </div>
-      //             <Link to={`/proposals`}>
-      //               <Button primary id="user-profile-browse">
-      //                 <Icon name="find" />Browse proposals
-      //               </Button>
-      //             </Link>
-
-      //           </List.Item>
-      //         </List>
-      //       </div>
-      //     )}
-
-      // </Container>
