2918bd93060be796ebb3785553b66a061cb78a6c
Yoni Slotwinere <yonislots@gmail.com>
2018-05-15 17:24:43 -0400

    added name to user db and seed; some styling to user profile

--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Header, Image, Item, Button, Container } from 'semantic-ui-react'
+import { Header, Image, Item, Button, Container, List, Icon } from 'semantic-ui-react'
 
 export default class UserProfile extends Component {
   constructor(props) {
@@ -16,17 +16,17 @@ export default class UserProfile extends Component {
     const projects = user.projects ? user.projects : []
     return (
       <Container>
-        <h1>Your Profile!</h1>
-        <div className="userContainer">
+        <h1 id="user-profile-title">{user.fullname}</h1>
+        <div>
           {user ? (
             <div>
-              <h3>Welcome {user.displayName || user.email}!</h3>
+              <h3 id="user-profile-email">{user.email}</h3>
               <Image src={user.imageUrl} />
             </div>
           ) : (
             <h3 className="loading">Loading current user...</h3>
           )}
-          <h2>Your current projects</h2>
+          <h2 id="user-profile-projects-header">Your Projects</h2>
 
           {projects.length ? (
             <Item.Group>
@@ -36,16 +36,16 @@ export default class UserProfile extends Component {
                     <div className="content">
                       <a className="header">{project.name}</a>
                       <div className="description">
-                        {project.description.slice(0, 80)}...
+                        {project.description}
                       </div>
                     </div>
                     <div>
                       <Link to={`/projects/${project.id}`}>
-                        <Button
+                        <Button primary id="user-profile-browse"
                           primary
                           onClick={evt => this.props.loadProject(project.id)}
                         >
-                          <Header as="h3">Check in on this project</Header>
+                          <Icon name="code" />Check in on this project
                         </Button>
                       </Link>
                     </div>
@@ -56,11 +56,11 @@ export default class UserProfile extends Component {
           ) : (
             <div>
               <h3>You currently have no active projects.</h3>
-              <Link to={`/proposals`}>
-                <Button primary>
-                  <Header as="h3">Click here to view all proposals-></Header>
-                </Button>
-              </Link>
+                <Link to={`/proposals`}>
+                  <Button primary id="user-profile-browse">
+                    <Icon name="search" />Browse proposals
+                  </Button>
+                </Link>
             </div>
           )}
         </div>
@@ -68,3 +68,71 @@ export default class UserProfile extends Component {
     )
   }
 }
+
+
+      // <Container>
+      //   <Header as="h1" icon textAlign="center">
+      //   {user ? (
+      //     <div>
+      //       <Header.Content id="user-profile-title">
+      //         {user.fullname || user.email}
+      //       </Header.Content>
+      //       <Header as="h1">
+      //         <Image size="massive" src={user.imageUrl} />
+      //       </Header>
+      //      </div>
+      //     ) : (
+      //       <h3 className="loading">Loading current user...</h3>
+      //     )}
+
+      //   </Header>
+
+      //     <h2 id="user-profile-projects-header">Your Projects</h2>
+
+      //     {projects.length ? (
+      //       <Item.Group>
+      //         {projects.map(project => {
+      //           return (
+      //             <Item key={project.id}>
+      //               <div className="content">
+      //                 <a className="header">{project.name}</a>
+      //                 <div className="description">
+      //                   {project.description.slice(0, 80)}...
+      //                 </div>
+      //               </div>
+      //               <div>
+      //                 <Link to={`/projects/${project.id}`}>
+      //                   <Button
+      //                     primary
+      //                     onClick={evt => this.props.loadProject(project.id)}
+      //                   >
+      //                     <Header as="h3">Check in on this project</Header>
+      //                   </Button>
+      //                 </Link>
+      //               </div>
+      //             </Item>
+      //           )
+      //         })}
+      //       </Item.Group>
+      //     ) : (
+      //       <div>
+      //         <List divided verticalAlign='middle'>
+      //           <List.Item>
+      //             <div className="no-active-projects">
+
+      //               <List.Content>
+      //                 You currently have no active projects
+      //               </List.Content>
+      //             </div>
+      //             <Link to={`/proposals`}>
+      //               <Button primary id="user-profile-browse">
+      //                 <Icon name="find" />Browse proposals
+      //               </Button>
+      //             </Link>
+
+      //           </List.Item>
+      //         </List>
+      //       </div>
+      //     )}
+
+      // </Container>
