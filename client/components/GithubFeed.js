aa1e263f28d1323773617ff46b6b86bf2fca78ec
Ian Dewsbury <ianmichaeld@gmail.com>
2018-05-16 10:58:38 -0400

    fix feed for pull requests

--- client/components/GithubFeed.js
+++ client/components/GithubFeed.js
@@ -23,31 +23,42 @@ export default class GithubFeed extends Component {
   render() {
     const { events } = this.props
     return (
-      <div>
+      <div className="feedDiv">
         <Header as="h3">Here are the most recent events for your repo: </Header>
         {events && events.length ? (
-          <Feed>
-            {events
-              .slice(events.length < 10 ? events.length : events.length - 10)
-              .reverse()
-              .map(event => {
-                return (
-                  <Feed.Event key={event.id}>
-                    <Feed.Label>
-                      <Icon circular color="teal" name="github alternate" />
-                    </Feed.Label>
-                    <Feed.Content>
-                      <Feed.Date>
-                        <TimeAgo datetime={event.updatedAt} />
-                      </Feed.Date>
-                      <Feed.Summary>
-                        <Feed.User>{event.githubUser}</Feed.User>
-                        <a href="event.url">{` ${event.description}`}</a>
-                      </Feed.Summary>
-                    </Feed.Content>
-                  </Feed.Event>
-                )
-              })}
+          <Feed id="Feed">
+            {events.reverse().map(event => {
+              return (
+                <Feed.Event key={event.id}>
+                  <Feed.Label>
+                    <Icon circular color="teal" name="github alternate" />
+                  </Feed.Label>
+                  <Feed.Content>
+                    <Feed.Date>
+                      <TimeAgo datetime={event.updatedAt} />
+                    </Feed.Date>
+                    <Feed.Summary>
+                      <Feed.User>{event.githubUser}</Feed.User>
+                      {event.type === 'push' && (
+                        <a target="_blank" href={event.url}>{` ${
+                          event.description
+                        }`}</a>
+                      )}
+                      {event.type === 'pull request' && (
+                        <span>
+                          {' '}
+                          {event.action} a{' '}
+                          <a
+                            target="_blank"
+                            href={event.url}
+                          >{` pull request`}</a>
+                        </span>
+                      )}
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+              )
+            })}
           </Feed>
         ) : (
           <Header as="h3">
