cf77584c4dcd049888d2b277bbbb8e275d474405
Yoni Slotwinere <yonislots@gmail.com>
2018-05-16 11:01:38 -0400

    org profile

--- client/components/OrganizationHome.js
+++ client/components/OrganizationHome.js
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom'
 export default class OrganizationHome extends Component {
   componentDidMount() {
     this.props.fetchCurrentOrg(this.props.currentUser.id)
+
     //refetches user to get the updated user after they are assigned to an org. Navbar checks if user is assigned to an org and renders out User Home vs Organization Home
     this.props.fetchUpdatedUser(this.props.currentUser.id)
   }
   render() {
     let { currentUser, currentOrg } = this.props
+    // *MIGHT NEED TO GET ORG ID FROM URL
     return (
       <Container>
         {currentOrg ? (
